f955bcc110ec0d2c45209e2f1863e5d8fdce1b19
fronomenal <94882375+fronomenal@users.noreply.github.com>
2023-01-16 23:37:28 +0000

    add alchemy models and pydantic schemas

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ import os
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi_sqlalchemy import DBSessionMiddleware, db
 from dotenv import load_dotenv
+
+from models import Author
+from models import Author as ModelAuthor
+from models import Book
+from models import Book as ModelBook
+from schema import Author as SchemaAuthor
+from schema import Book as SchemaBook
 
 load_dotenv("./env_files/api.env")
 
