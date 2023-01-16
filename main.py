5fa68d95415a9225c64e3bb246a0cb90524e2ea1
fronomenal <94882375+fronomenal@users.noreply.github.com>
2023-01-16 23:50:26 +0000

    add basic routes for books and authors

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ from fastapi_sqlalchemy import DBSessionMiddleware, db
 from dotenv import load_dotenv
 
 from models import Author
-from models import Author as ModelAuthor
 from models import Book
-from models import Book as ModelBook
 from schema import Author as SchemaAuthor
 from schema import Book as SchemaBook
 
@@ -16,9 +14,40 @@ load_dotenv("./env_files/api.env")
 
 app = FastAPI()
 
-@app.get("/testing")
+app.add_middleware(DBSessionMiddleware, db_url=os.getenv("DATABASE_URL"))
+
+@app.get("/")
 async def root():
-    return {"message": os.getenv("TESTING")}
+    return {"message": "Welcome to the Fast Bookstore"}
+
+
+@app.get("/books/")
+def get_books():
+    books = db.session.query(Book).all()
+    return books
+
+@app.get("/authors/")
+def get_authors():
+    books = db.session.query(Author).all()
+    return books
+
+
+@app.post("/books/", response_model=SchemaBook)
+def add_book(book: SchemaBook):
+    db_book = Book(title=book.title, rating=book.rating, author_id=book.author_id)
+    db.session.add(db_book)
+    db.session.commit()
+    return db_book
+
+
+@app.post("/authors/", response_model=SchemaAuthor)
+def add_author(author: SchemaAuthor):
+    db_author = Author(name=author.name, age=author.age)
+    db.session.add(db_author)
+    db.session.commit()
+    return db_author
+
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
