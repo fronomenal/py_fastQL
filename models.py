b9f9ad56484bc15fc1577ac2908dba6726da528e
fronomenal <94882375+fronomenal@users.noreply.github.com>
2023-02-19 17:50:45 +0000

    create and migrate  User, Post models

--- models.py
+++ models.py
@@ -1,9 +1,8 @@
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
-Base = declarative_base()
+from db_conf import Base
 
 class Book(Base):
     __tablename__ = "book"
