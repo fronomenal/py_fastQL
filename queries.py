0ab3f01847a4da33e955748a2eeca5441dc643ed
fronomenal <94882375+fronomenal@users.noreply.github.com>
2023-02-19 23:47:09 +0000

    add authentication to graphql endpoint

--- queries.py
+++ queries.py
@@ -1,7 +1,13 @@
+from datetime import timedelta
+
 import graphene
+import bcrypt
+from graphql import GraphQLError
+from jwt import PyJWTError
+from utils import create_access_token, decode_access_token
 
 import models
-from schemas import PostModel, PostSchema
+from schemas import PostModel, PostSchema, UserSchema
 from db_conf import db_session
 
 db = db_session.session_factory()
@@ -18,31 +24,96 @@ class Query(graphene.ObjectType):
   def resolve_post_by_id(self, info, post_id):
       return db.query(models.Post).filter(models.Post.id == post_id).first()
 
+class AuthenticateUser(graphene.Mutation):
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+
+    ok = graphene.Boolean()
+    token = graphene.String()
+
+    @staticmethod
+    def mutate(root, info, username, password):
+
+        user = UserSchema(username=username, password=password)
+
+        db_user_info = db.query(models.User).filter(models.User.username == username).first()
+
+        if bcrypt.checkpw(user.password.encode("utf-8"), db_user_info.password.encode("utf-8")):
+          access_token_expires = timedelta(minutes=60)
+          access_token = create_access_token(data={"user": username}, expires_delta=access_token_expires)
+          return AuthenticateUser(ok=True, token=access_token)
+        return AuthenticateUser(ok=False)
+
+
+class CreateNewUser(graphene.Mutation):
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+
+    ok = graphene.Boolean()
+
+    @staticmethod
+    def mutate(root, info, username, password):
+
+        hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+        password_hash = hashed_password.decode("utf8")
+
+        user = UserSchema(username=username, password=password_hash)
+        db_user = models.User(username=user.username, password=password_hash)
+        try:
+          db.add(db_user)
+          db.commit()
+          db.refresh(db_user)
+          ok = True
+          return CreateNewUser(ok=ok)
+        except:
+          db.rollback()
+          db.close()
+          raise
+
 
 class CreateNewPost(graphene.Mutation):
   class Arguments:
       title = graphene.String(required=True)
       content = graphene.String(required=True)
       author = graphene.String()
-      userId = graphene.Int(required=True)
+      token = graphene.String(required=True)
 
   ok = graphene.Boolean()
 
   @staticmethod
-  def mutate(root, info, title, content, author, userId):
-      post = PostSchema(title=title, content=content, author=author, user_id=userId)
-      db_post = models.Post(title=post.title, content=post.content, author=post.author, user_id=post.user_id)
-      try:
-        db.add(db_post)
-        db.commit()
-        db.refresh(db_post)
-        return CreateNewPost(ok=True)
-      except:
-        db.rollback()
-        return CreateNewPost(ok=False)
+  def mutate(root, info, title, content, author, token):
+    try:
+      payload = decode_access_token(data=token)
+      username = payload.get("user")
+      if username is None:
+        raise GraphQLError("Invalid credentials")
+    except PyJWTError:
+      raise GraphQLError("Invalid token provided")
+
+    user = db.query(models.User).filter(models.User.username == username).first()
+
+    if user is None:
+      raise GraphQLError("Invalid user credentials")
+    
+    post = PostSchema(title=title, content=content, author=author, user_id=user.user_id)
+    db_post = models.Post(title=post.title, content=post.content, author=post.author, user_id=post.user_id)
+      
+    try:
+      db.add(db_post)
+      db.commit()
+      db.refresh(db_post)
+      return CreateNewPost(ok=True)
+    except:
+      db.rollback()
+      return CreateNewPost(ok=False)
 
 
 class PostMutations(graphene.ObjectType):
+  authenticate_user = AuthenticateUser.Field()
+  create_new_post = CreateNewPost.Field()
   create_new_post = CreateNewPost.Field()
   
 
