2e00fae2d194c5e8b76f05e192b887173987eb92
fronomenal <94882375+fronomenal@users.noreply.github.com>
2023-03-13 01:15:44 +0000

    fix errors with graphql mutations

--- queries.py
+++ queries.py
@@ -5,6 +5,7 @@ import bcrypt
 from graphql import GraphQLError
 from jwt import PyJWTError
 from utils import create_access_token, decode_access_token
+from pydantic import ValidationError
 
 import models
 from schemas import PostModel, PostSchema, UserSchema
@@ -47,44 +48,49 @@ class AuthenticateUser(graphene.Mutation):
 
 
 class CreateNewUser(graphene.Mutation):
-    class Arguments:
-        username = graphene.String(required=True)
-        password = graphene.String(required=True)
+  class Arguments:
+    username = graphene.String(required=True)
+    password = graphene.String(required=True)
 
-    ok = graphene.Boolean()
+  ok = graphene.Boolean()
+  msg = graphene.String()
 
-    @staticmethod
-    def mutate(root, info, username, password):
+  @staticmethod
+  def mutate(root, info, username, password):
 
-        hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+    hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
-        password_hash = hashed_password.decode("utf8")
+    password_hash = hashed_password.decode("utf8")
+    
+    try:
+      user = UserSchema(username=username, password=password_hash)
+      db_user = models.User(username=user.username, password=password_hash)
+    except  ValidationError as e:
+      return CreateNewUser(ok=False, msg=e)
 
-        user = UserSchema(username=username, password=password_hash)
-        db_user = models.User(username=user.username, password=password_hash)
-        try:
-          db.add(db_user)
-          db.commit()
-          db.refresh(db_user)
-          ok = True
-          return CreateNewUser(ok=ok)
-        except:
-          db.rollback()
-          db.close()
-          raise
+    try:
+      db.add(db_user)
+      db.commit()
+      db.refresh(db_user)
+      return CreateNewUser(ok=True, msg=user.username)
+    except:
+      db.rollback()
+      db.close()
+      return CreateNewUser(ok=False)
 
 
 class CreateNewPost(graphene.Mutation):
   class Arguments:
       title = graphene.String(required=True)
       content = graphene.String(required=True)
-      author = graphene.String()
       token = graphene.String(required=True)
+      author = graphene.String()
 
   ok = graphene.Boolean()
+  msg = graphene.String()
 
   @staticmethod
-  def mutate(root, info, title, content, author, token):
+  def mutate(root, info, title, content, token, author=""):
     try:
       payload = decode_access_token(data=token)
       username = payload.get("user")
@@ -98,9 +104,12 @@ class CreateNewPost(graphene.Mutation):
     if user is None:
       raise GraphQLError("Invalid user credentials")
     
-    post = PostSchema(title=title, content=content, author=author, user_id=user.user_id)
-    db_post = models.Post(title=post.title, content=post.content, author=post.author, user_id=post.user_id)
-      
+    try:
+      post = PostSchema(title=title, content=content, author=author, user_id=user.id)
+      db_post = models.Post(title=post.title, content=post.content, author=post.author, user_id=post.user_id)
+    except  ValidationError as e:
+      return CreateNewPost(ok=False, msg=e)
+
     try:
       db.add(db_post)
       db.commit()
@@ -108,12 +117,12 @@ class CreateNewPost(graphene.Mutation):
       return CreateNewPost(ok=True)
     except:
       db.rollback()
-      return CreateNewPost(ok=False)
+      return CreateNewPost(ok=False, msg=post.content)
 
 
 class PostMutations(graphene.ObjectType):
   authenticate_user = AuthenticateUser.Field()
-  create_new_post = CreateNewPost.Field()
+  create_new_user = CreateNewUser.Field()
   create_new_post = CreateNewPost.Field()
   
 
